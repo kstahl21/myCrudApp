ed7c2c625dbb1f1fbf487e899778ed30ae464018
Kevin Stahl <kstahl21@gmail.com>
2018-06-01 16:14:03 -0700

    Implemented a working solution to add snippets, search for snippets, and delete snippets to and from localStorage

---
+++ app.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+	$('.submitSnippet').on('click', function(){
+		let snippetName = $('.snippetName').val();
+		let snippetCode = $('.snippetCode').val();
+
+		localStorage.setItem(snippetName, snippetCode);
+		$('.snippetName').val('')
+		$('.snippetCode').val('')
+	});
+
+
+	$('.searchButton').on('click', function(){
+
+		let searchRequest = $('.snippetSearchText').val();
+
+		var result = false;
+
+		for (i = 0; i < localStorage.length; i++){
+			var snippetTitle = localStorage.key(i);
+			var snippetCode = localStorage.getItem(localStorage.key(i))
+				if (snippetTitle === searchRequest){
+					result = true;
+					$('.snippetCodeResult').text(snippetCode);
+				}
+		}
+
+			if (result === false) {
+				$('.snippetCodeResult').text('Sorry this snippet you searched for does not exist. Maybe you should try writing it for us');
+			}
+
+		
+	});
+
+
+	$('.listAllButton').on('click',function(){
+		var listOfAllSnippets = ""
+		for (var i = 0; i < localStorage.length; i++){
+			var snippetTitle = localStorage.key(i);
+    		var snippetCode = localStorage.getItem(localStorage.key(i));
+    		listOfAllSnippets+= (`This snippet is called ${snippetTitle}\n ${snippetCode}\n \n`)
+		}
+
+		$('.snippetCodeResult').text(listOfAllSnippets);
+
+	});
+
+	$('.deleteButton').on('click',function(){
+
+		var snippetToBeRemoved = $('.deleteName').val();
+		localStorage.removeItem(snippetToBeRemoved);
+
+	});
+
+});
+
+
